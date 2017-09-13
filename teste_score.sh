8ef23e6588d359438d65a60701c550ed2f1efc7d
gustavo <cetres@gmail.com>
2017-09-13 20:56:54 -0300

    ajuste no teste de commit

--- teste_score.sh
+++ teste_score.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "Teste de obtencao de score"
 curl http://127.0.0.1:5000/score/82342345623
+echo "Teste de avaliacao de acesso"
 curl http://127.0.0.1:5000/score/82342345623?acao=dados_segurado
